6ff2d484b0dba6a5cd28f038c4185f7f68d066fe
John Shahid <jvshahid@gmail.com>
2017-10-26 17:04:04 -0400

    wip

---
+++ src/code.cloudfoundry.org/lds/cmd/lds/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	s := &http.Server{
+		Addr: ":9933",
+		Handler: http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			f, err := ioutil.ReadFile("/etc/envoy_config/listeners.json")
+			if err != nil {
+				panic(err)
+			}
+			rw.Write(f)
+		}),
+	}
+	s.ListenAndServe()
+	panic("shouldn't be here")
+}
