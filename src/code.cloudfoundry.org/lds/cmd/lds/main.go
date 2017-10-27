d620035ccbe72dde2c869c449c8453f049f9e50d
Brandon Shroyer <bshroyer@pivotal.io>
2017-10-27 17:48:01 -0400

    wip

--- src/code.cloudfoundry.org/lds/cmd/lds/main.go
+++ src/code.cloudfoundry.org/lds/cmd/lds/main.go
@@ -9,7 +9,7 @@ func main() {
 	s := &http.Server{
 		Addr: ":9933",
 		Handler: http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			f, err := ioutil.ReadFile("/etc/envoy_config/listeners.json")
+			f, err := ioutil.ReadFile("/etc/cf-assets/envoy_config/listeners.json")
 			if err != nil {
 				panic(err)
 			}
